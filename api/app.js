c0b12117fd02b524e09f63d583e11309f57b7a63
Marina <lahniuk.m@gmail.com>
2020-12-19 05:12:38 +0300

    add catching errors, add .env

--- api/app.js
+++ api/app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+const express = require('express');
+const app = express();
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World! I\'m going to develop a new social network')
-})
+  res.send('Hello World! I\'m going to develop a new social network');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Server has some problems');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
